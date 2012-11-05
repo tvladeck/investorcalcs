7b0f4a0333eabbc127a055d911f1fd3a93a9079e
Tom Vladeck <thomas.vladeck@gmail.com>
2012-11-05 07:47:35 -0800

    annual IRR & individual deals now in SimulateDeals

--- calc.js
+++ calc.js
@@ -85,34 +85,35 @@ function simulateDeals (N, upfrontCost, paybackMonths, attritionRate,
         var revenueStream = [];
         for (var i = 0; i < months; i++) { revenueStream.push(0); }
 
-        console.log('check 1');
+        var individualResults = [];
         for (var i = 0; i < N; i++)
         {
                 var feeVector = contractStream(fee, attritionRate, renewalRate, contractLength, months);
+                individualResults.push(feeVector);
                 for (var j = 0; j < months; j++)
                 {
                         revenueStream[j] += feeVector[j];
                 }
         }
 
-        console.log('check 2');
         var totalRevenue = 0;
         for (var i = 0; i < months; i++)
         {
                 totalRevenue += revenueStream[i];
         }
 
-        console.log('check 3');
-        var totalVector = revenueStream;
-        totalVector.unshift(-totalUpfront);
+        var totalVector = revenueStream; // new variable to calculate IRR
+        totalVector.unshift(-totalUpfront); // need to add upfront cost
         var returns = irr(totalVector);
+        returns = Math.pow((1 + returns), 12) - 1; // convert monthly IRR to annual
 
         var result =
         {
                 capex: totalUpfront,
                 irr: returns,
                 stream: revenueStream,
-                totalRevenue: totalRevenue
+                totalRevenue: totalRevenue,
+                projects: individualResults
         };
 
         return result;
