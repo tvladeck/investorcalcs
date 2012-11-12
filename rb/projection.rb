6f42cf7730e515477b11a90e0c6371ddc2a2f53a
Tom Vladeck <thomas.vladeck@gmail.com>
2012-11-11 16:01:58 -0800

    added lamp lifetime as a parameter.
    it affects the capex value

--- rb/projection.rb
+++ rb/projection.rb
@@ -20,6 +20,8 @@ class Projection
 ##  salesToOps: ratio of sales to operations people
 ##  opsSalary: salary of ops people;
 ##  opex_ratio: opex expenses as ratio of salary
+##  lamp replace years: the number of years after which we will replace the
+    #lamps. given as an average value
     @upfront_cost         = options[:upfront_cost] || 2000
     @payback_months       = options[:payback_months] || 14
     @attrition_rate       = options[:attrition_rate] || 0.03
@@ -35,6 +37,7 @@ class Projection
     @sales_to_ops         = options[:sales_to_ops] || 10
     @ops_sal              = options[:ops_sal] || 5000
     @opex_ratio           = options[:opex_ratio] || 0.25
+    @lamp_replace_years   = options[:lamp_replace_years] || 5
   end
 
 
@@ -47,7 +50,6 @@ class Projection
 ##  capex: just capex
 ##  salaries: just salaries
 
-    attrition_rate  = 1 - ((1 - @attrition_rate) ** (1 / 12.0))
     revenues        = []
     capex           = []
     salaries        = []
@@ -57,10 +59,11 @@ class Projection
     sales           = @initial_sales
     ops             = [1, (sales / @sales_to_ops).floor].max
     total_months    = @stop_months + @pad_months
-    total_months.times do 
-      revenues << 0 
-      profit   << 0
-      cashflow << 0
+    total_months.times do
+      revenues  << 0
+      profit    << 0
+      cashflow  << 0
+      capex     << 0
     end
 
     @stop_months.times do |month|
@@ -72,23 +75,25 @@ class Projection
       opex_month = @opex_ratio * salary
       num_deals = @deals_per_sales * sales
       months_ahead = total_months - month
-      simulation = simulate_deals(num_deals, months_ahead)
-      capex << simulation[:capex]
+
       salaries << salary
       opex << opex_month
 
-      revs = simulation[:revenues]
+      simulation = simulate_deals(num_deals, months_ahead)
+      cap   = simulation[:capex]
+      revs  = simulation[:revenues]
       month.times do
         revs.unshift(0)
+        cap.unshift(0)
       end
       total_months.times do |m|
         revenues[m] += revs[m]
+        capex[m]    += cap[m]
       end
     end
 
     @pad_months.times do
       salaries  << 0
-      capex     << 0
       opex      << 0
     end
 
@@ -105,34 +110,51 @@ class Projection
     total_upfront = n * @upfront_cost
     fee = @upfront_cost / @payback_months
     revenue_stream = []
-    months.times do revenue_stream << 0 end
+    capex = []
+    months.times do
+      revenue_stream  << 0
+      capex           << 0
+    end
 
-    n.times do |deal|
-      fee_vector = contract_stream(months)
+    n.times do
+      deal        = contract_stream(months)
+      fee_vector  = deal[:fee_vector]
+      cap         = deal[:capex]
       fee_vector.each_with_index do |fee, index|
         revenue_stream[index] += fee
       end
+      cap.each_with_index do |cap, index|
+        capex[index] += cap
+      end
     end
 
-    result = { :capex => total_upfront, :revenues => revenue_stream }
+    result = { :capex => capex, :revenues => revenue_stream }
     result
   end
 
   def contract_stream(months)
-    fee = @upfront_cost / @payback_months
-    fee_vector = []
-    cancel_rate = 1 - @renewal_rate
+    fee                 = @upfront_cost / @payback_months
+    attrition_rate      = 1 - ((1 - @attrition_rate) ** (1 / 12.0))
+    lamp_replace_months = @lamp_replace_years * 12
+    fee_vector          = []
+    capex               = []
+    cancel_rate         = 1 - @renewal_rate
     months.times do |month|
       if month % @contract_length == 0 && month != 0 && Random.rand < cancel_rate
         break
-      elsif Random.rand < @attrition_rate
+      elsif Random.rand < attrition_rate
         break
       else
         fee_vector << fee
       end
+      if month % lamp_replace_months == 0
+        capex << @upfront_cost
+      else
+        capex << 0
+      end
     end
 
-    fee_vector
+    {:fee_vector => fee_vector, :capex => capex}
   end
 
 end
